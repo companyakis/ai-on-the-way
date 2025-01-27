94bbbe45ec668b1e3ae0dc9f4e60016b6517a77a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-27 13:19:02 +0300

    Create part-statistics-0001-sampling2.py

---
+++ part-statistics-0001-sampling2.py
@@ -0,0 +1,12 @@
+sample_50_ages = np.random.choice(a = people_ages, size = 50)
+
+print(sample_50_ages.size) # 50
+
+print(sample_50_ages)
+
+"""
+[18 37 35  2  2 60 38 11 25 77 59 73 88 64 68 22 47  8 56 20 78 92 25 88
+ 26 39  6  2 26 75  5 30 64 92 30 16 61 55 72 83 56 33 53 27 58 93 28 87
+  2 53]
+
+"""
