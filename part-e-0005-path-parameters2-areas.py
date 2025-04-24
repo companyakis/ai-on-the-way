78668dc78e28608555ee0afdd9d6765549d68faf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-24 22:20:10 +0300

    Update part-e-0005-path-parameters2-areas.py

--- part-e-0005-path-parameters2-areas.py
+++ part-e-0005-path-parameters2-areas.py
@@ -15,14 +15,14 @@ UNITS = [
 async def company_units():
     return UNITS
 
-@app.get("/departments/{dep_id}")
-async def return_department_by_id(dep_id: str):
-    for unit in UNITS:
-        if unit["id"].casefold() == dep_id.casefold():
-            return unit
+# @app.get("/departments/{dep_id}")
+# async def return_department_by_id(dep_id: str):
+#     for unit in UNITS:
+#         if unit["id"].casefold() == dep_id.casefold():
+#             return unit
         
-@app.get("/departments/{man}")
-async def get_department_by_manager(man: str):
+@app.get("/departments/{manager}")
+async def get_department_by_manager(manager: str):
     for unit in UNITS:
-        if unit["manager"].casefold() == man.casefold():
+        if unit["manager"].casefold() == manager.casefold():
             return unit
