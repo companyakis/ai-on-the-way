70329e002110490e2a953ce388ac7f150d0fd146
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-04-26 22:53:41 +0300

    Create part-e-0009-crud-delete1-areas.py

---
+++ part-e-0009-crud-delete1-areas.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+UNITS = [
+    {"id": "1", "area": "FinTech", "manager": "Mustafa Buyukdereli", "number of employees": 12, "location": "x city"},
+    {"id": "2", "area": "Sales", "manager": "Aygun Arslan", "number of employees": 7, "location": "istanbul"},
+    {"id": "6", "area": "Marketing", "manager": "Bilge Kağan", "number of employees": 4, "location": "istanbul"},
+    {"id": "8", "area": "Finance", "manager": "Bengü Bilici", "number of employees": 3, "location": "istanbul"},
+    {"id": "3", "area": "Operations", "manager": "Kutluk Gokturk", "number of employees": 3, "location": "x city"},
+    {"id": "5", "area": "Audit", "manager": "Kultigin Yapici", "number of employees": 2, "location": "x city"},
+]
+
+@app.get("/departments")
+async def company_units():
+    return UNITS
+
+# delete a unit by id
+
+@app.delete("delete-unit/{unit_id}")
+async def delete_dep_by_id(unit_id: str):
+    for i in range(len(UNITS)):
+        if UNITS[i]["id"].casefold() == unit_id.casefold():
+            UNITS.pop(i)
+            break 
+
+# update a department by id num 
+
+@app.put("/update-department")
+async def update_a_dep(updated_dep = Body()):
+    for i in range(len(UNITS)):
+        if UNITS[i]["id"].casefold() == updated_dep["id"].casefold():
+            UNITS[i] = updated_dep
+
+
+# add a new department
+
+@app.post("/add-new-unit")
+async def add_new_department(new_dep = Body()):
+    UNITS.append(new_dep)
+
