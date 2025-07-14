3761e97dfc75627e1b612324fd2c66520e68c28e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-14 23:53:41 +0300

    Update part-t-0005-simple-nn-model1.py

--- part-t-0005-simple-nn-model1.py
+++ part-t-0005-simple-nn-model1.py
@@ -2,14 +2,14 @@ import torch
 from torch import nn 
 
 # profit = 2 * sales - 300
-# y = 2 * X - 30000
+# y = 2 * X - 30_000
 # first_quarter_sales = torch.tensor([55_600, 62_000, 48_600])
 # fixed_cost_per_month = torch.tensor(30_000)
 
 X = torch.tensor([
-    [55_600],
-    [62_000],
-    [48_600]
+    [55_600.0],
+    [62_000.0],
+    [48_600.0]
 ])
 
 profit_forecast_model = nn.Linear(1, 1)
