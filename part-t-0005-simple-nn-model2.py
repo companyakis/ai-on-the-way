cc80ff55b3d79009a43e81e9e30d91e7677b6a83
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-15 00:00:47 +0300

    Update part-t-0005-simple-nn-model2.py

--- part-t-0005-simple-nn-model2.py
+++ part-t-0005-simple-nn-model2.py
@@ -1,6 +1,6 @@
 profit_forecast_model.bias = nn.Parameter(torch.tensor([-30000.0]))
 
-profit_forecast_model.weight = nn.Parameter(torch.tensor([2.0]))
+profit_forecast_model.weight = nn.Parameter(torch.tensor([[2.0]]))
 
 print(profit_forecast_model.bias)
 
@@ -9,8 +9,7 @@ print(profit_forecast_model.weight)
 """
 Parameter containing:
 tensor([-30000.], requires_grad=True)
-
 Parameter containing:
-tensor([2.], requires_grad=True)
+tensor([[2.]], requires_grad=True)
 
 """
